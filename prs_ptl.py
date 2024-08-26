36397719dd397a689b43459bc626ebbdbe8e5c52
Blaj3n <benceulrich@gmail.com>
2024-08-26 06:25:33 +0200

    feladatleírás

---
+++ prs_ptl.py
@@ -0,0 +1,13 @@
+# Feladat leírása: Írj egy programot, amely a felhasználótól bekér egy pozitív egész számot, majd kiszámolja és
+# kiírja a következőket:
+#
+# Az 1 és a megadott szám közötti páros számok összegét.
+# Az 1 és a megadott szám közötti páratlan számok összegét.
+# Példa bemenet:
+#
+# Adjon meg egy pozitív egész számot: 10
+#
+# Példa kimenet:
+#
+# Páros számok összege: 30
+# Páratlan számok összege: 25
